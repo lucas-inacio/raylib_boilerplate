a567c5111c76b74fe90e3d65bb39cdc1a18d92e4
Lucas Inácio Viegas <lucas.viegas@edu.pucrs.br>
2022-05-29 16:41:48 -0300

    Removed CMAKE_ARGS in FetchContent as it does not have effect

--- cmake/raylib.cmake
+++ cmake/raylib.cmake
@@ -3,7 +3,6 @@ include(FetchContent)
 FetchContent_Declare(
     raylib_external
     GIT_REPOSITORY https://github.com/raysan5/raylib.git
-    CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${CMAKE_SOURCE_DIR}/external/raylib
 )
 
 FetchContent_MakeAvailable(raylib_external)
